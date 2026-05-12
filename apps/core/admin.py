b6ed14832e44198c2f6c440d5d8085d0269a0a28
Zillgates <zillgates522@gmail.com>
2026-05-12 10:20:57 -0700

    changes admin

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.site_header = "Veta Admin"
+admin.site.site_title = "Veta Admin Dashboard"
+admin.site.index_title = "Welcome to Hotel System"
